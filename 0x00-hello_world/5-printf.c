060de289abb5818b17db25e042f92573356f021e
Opeoluwa-A <ahmedopeoluwa@gmail.com>
2022-03-13 15:59:02 -0700

    C for the first time

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+/**
+ * main - prints the body
+ *Description: printf will print the desired output
+ * Return: the program should return 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is  piece of art,\n");
+	return (0);
+}
